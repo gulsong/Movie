651a8268a98002ec315a7d6cfd809840af329e7a
gulsong <3504804157@qq.com>
2021-05-20 19:35:23 +0800

    add createComponents

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,91 +1,93 @@
 <template>
-    <div class="cinema_body">
-        <ul>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <span>嘉星国际影城(会展店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
-                </div>
-                <div class="address">
-                    <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
-                    <span>1763.5km</span>
-                </div>
-                <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
-                </div>
-            </li>
-        </ul>
+    <div class="cinema_b">
+        <div class="cinema_body">
+            <ul>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <span>嘉星国际影城(会展店)</span>
+                        <span class="q"><span class="price">22.9</span> 元起</span>
+                    </div>
+                    <div class="address">
+                        <span>南岗区红旗大街240号(艺汇家文化商业广场3楼)</span>
+                        <span>1763.5km</span>
+                    </div>
+                    <div class="card">
+                        <div>小吃</div>
+                        <div>折扣卡</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -96,6 +98,7 @@ export default {
 </script>
 
 <style scoped>
+    #content .cinema_b{ margin-top: 45px; display: flex; width:100%; position: absolute; top: 50px; bottom: 50px;}
     #content .cinema_body{ flex:1; overflow:auto;}
     .cinema_body ul{ padding:20px;}
     .cinema_body li{  border-bottom:1px solid #e6e6e6; margin-bottom: 20px;}
